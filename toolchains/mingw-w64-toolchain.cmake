7b00090abb7b7a13b7f8fbe5129bcee05fc53afd
Vincenzo Barbato <navyenzo@gmail.com>
2023-12-06 09:29:59 -0800

    making changes to allow cross-compilation for windows

---
+++ toolchains/mingw-w64-toolchain.cmake
@@ -0,0 +1,11 @@
+# Set the system name to ensure CMake uses the correct tools and libraries
+set(CMAKE_SYSTEM_NAME Windows)
+
+# Specify the cross-compiler
+set(CMAKE_C_COMPILER /usr/bin/x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/x86_64-w64-mingw32-g++)
+
+# Configure CMake to use the cross-compiler for finding libraries and headers
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
